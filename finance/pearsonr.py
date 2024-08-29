4d8e040c14171886bc3f563151d91cfff46377a2
buhe <bugu1986@126.com>
2024-08-29 17:03:59 +0800

    "Refactor date calculations and update stock ticker symbol in finance/pearsonr.py."

--- finance/pearsonr.py
+++ finance/pearsonr.py
@@ -3,13 +3,13 @@ from scipy.stats import pearsonr
 import datetime
 
 # 设置时间段：最近一年
-end_date = datetime.datetime.today()
-start_date = end_date - datetime.timedelta(days=100)
+end_date = datetime.datetime.today() - datetime.timedelta(days=100)
+start_date = end_date - datetime.timedelta(days=3650)
 
 # 下载纳斯达克金龙中国指数（HXC）和标普500指数（^GSPC）的数据
-hxc = yf.download('^TNX', start=start_date, end=end_date)['Adj Close']
+hxc = yf.download('KWEB', start=start_date, end=end_date)['Adj Close']
 sp500 = yf.download('SPY', start=start_date, end=end_date)['Adj Close']
-print(((hxc.head() + 100) / 100) ** 10)
+print(hxc.head())
 # 计算每日收益率
 hxc_returns = hxc.pct_change().dropna()
 sp500_returns = sp500.pct_change().dropna()
