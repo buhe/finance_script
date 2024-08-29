4ed0147e917b7c234823b7fc8245284ce09a608e
buhe <bugu1986@126.com>
2024-08-29 17:03:18 +0800

    "Refactor asset allocation calculations and add bond and CD components."

--- finance/assetAllocation.py
+++ finance/assetAllocation.py
@@ -13,28 +13,36 @@ sp500_first = sp500.iloc[0]
 cn300 = yf.download('ASHR', start=start_date, end=end_date)['Adj Close']
 cn300_first = cn300.iloc[0]
 
-tickers = ['KWEB', 'SPY', '600519.SS', '600036.SS', 'GLD', 'ASHR', '511260.SS', '^TNX', 'AAPL', 'TSLA', 'TCEHY', 'API', 'SPY', 'TLT', 'SGOV', 'VGIT']
-weight = [0.05, 0.19, 0.017, 0.008, 0.03, 0.015, 0.01, 0.23, 0.01 * 2, 0.01, 0.01, 0.05, 0.19, 0.07, 0.09, 0.02]
+tickers = ['KWEB', 'SPY', '600519.SS', '600036.SS', 'GLD', 'ASHR', '511260.SS', 'AAPL', 'TSLA', 'TCEHY', 'API', 'SPY', 'TLT', 'SGOV', 'VGIT']
+weight = [0.05, 0.19, 0.017, 0.008, 0.03, 0.015, 0.01, 0.01 * 2, 0.01, 0.01, 0.05, 0.19, 0.07, 0.09, 0.02]
 weight_index = 0
 sum = 0
 
 for ticker in tickers:
     data = yf.download(ticker, start=start_date, end=end_date)['Adj Close']
     first = data.iloc[0]
-    print(f"{ticker} data:")
+    # print(f"{ticker} data:")
     # print(first) # 打印第一个日期的收盘价
     # print(data.head())
     # print(data.head() / first)
     result = data / first * weight[weight_index]
     weight_index = weight_index + 1 
-    print(result.head())
+    # print(result.head())
     sum = sum + result
+
+print("cd data:")
+cd_raw = yf.download('^TNX', start=start_date, end=end_date)['Adj Close']
+cd_raw = (((cd_raw + 100) / 100) ** 1)
+# TODO 债劵的分红
+# TODO cd 的复利
+cd = cd_raw ** 10 * 0.23
+
 
 real_estate = 0.18
 # 假设房地产和货币基金不涨不跌
 # 绘制收盘价折线图
 plt.figure(figsize=(10, 6))
-plt.plot(sum + real_estate, label=f'Asset Allocation')
+plt.plot(sum + real_estate + cd, label=f'Asset Allocation')
 plt.plot(sp500 / sp500_first, label=f'S&P 500')
 plt.plot(cn300 / cn300_first, label=f'CN 300')
 plt.title(f'Asset Allocation vs Other Indexes')
