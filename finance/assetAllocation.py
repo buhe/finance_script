7a9d0aabd471e334fc11df79af43cf072c6ae0d0
buhe <bugu1986@126.com>
2024-08-29 15:36:23 +0800

    "Refactor asset allocation code to include additional tickers and corresponding weights, and plot S&P 500 index for comparison."

--- finance/assetAllocation.py
+++ finance/assetAllocation.py
@@ -7,16 +7,21 @@ import datetime
 end_date = datetime.datetime.today() - datetime.timedelta(days=100)
 start_date = end_date - datetime.timedelta(days=3650)
 
-tickers = ['KWEB', 'SPY', '600519.SS', '600036.SS']
-weight = [0.05, 0.19, 0.017, 0.008]
+sp500 = yf.download('SPY', start=start_date, end=end_date)['Adj Close']
+sp500_first = sp500.iloc[0]
+tickers = ['KWEB', 'SPY', '600519.SS', '600036.SS', 'GLD', 'ASHR', '511260.SS', '^TNX']
+weight = [0.05, 0.19, 0.017, 0.008, 0.03, 0.015, 0.01, 0.23]
 weight_index = 0
 sum = 0
 
 for ticker in tickers:
     data = yf.download(ticker, start=start_date, end=end_date)['Adj Close']
+    first = data.iloc[0]
     print(f"{ticker} data:")
-    print(data.head())
-    result = data * weight[weight_index]
+    # print(first) # 打印第一个日期的收盘价
+    # print(data.head())
+    # print(data.head() / first)
+    result = data / first * weight[weight_index]
     weight_index = weight_index + 1 
     print(result.head())
     sum = sum + result
@@ -24,6 +29,7 @@ for ticker in tickers:
 # 绘制收盘价折线图
 plt.figure(figsize=(10, 6))
 plt.plot(sum, label=f'Asset Allocation')
+plt.plot(sp500 / sp500_first, label=f'S&P 500')
 plt.title(f'Asset Allocation vs Other Indexes')
 plt.xlabel('Date')
 plt.ylabel('Price (USD)')
