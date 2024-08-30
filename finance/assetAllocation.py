8a26b80a2df9e32a11097f2331b1279a6299d63d
buhe <bugu1986@126.com>
2024-08-30 15:38:33 +0800

    "Update asset allocation weights in assetAllocation.py."

--- finance/assetAllocation.py
+++ finance/assetAllocation.py
@@ -30,7 +30,7 @@ cn300 = yf.download('ASHR', start=start_date, end=end_date)['Adj Close']
 cn300_first = cn300.iloc[0]
 
 tickers = ['KWEB', 'VOO', '600519.SS', '600036.SS', 'GLD', 'ASHR', '511260.SS', 'AAPL', 'TSLA', 'TCEHY', 'API', 'TLT', 'SGOV', 'VGIT']
-weight = [0.05, 0.19, 0.017, 0.008, 0.03, 0.015, 0.01, 0.015, 0.01, 0.015, 0.05, 0.07, 0.09, 0.02]
+weight = [0.03, 0.21, 0.017, 0.008, 0.03, 0.015, 0.01, 0.015, 0.01, 0.015, 0.05, 0.07, 0.09, 0.02]
 assert len(tickers) == len(weight)
 weight_index = 0
 sum = 0
